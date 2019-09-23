8ce878fb090ebb5dcd51474f8b45149649d293d7
varsnotwars <varsnotwars@protonmail.com>
2019-09-23 13:29:51 +1000

    Login mutation working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
 
-class App extends React.Component {
-  render() {
-    return <div className="App">Hello world!</div>;
-  }
-}
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
+import { Login } from "./Components/Login";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  credentials: "include"
+});
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <Login />
+  </ApolloProvider>
+);
 
 export default App;
---
+++ src/Components/Login.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { useMutation } from "@apollo/react-hooks";
+
+import { Button, Form, FormGroup, Input } from "reactstrap";
+
+import { LOGIN } from "../graphql/login";
+
+export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [login, { called, loading, client }] = useMutation(LOGIN);
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-6 offset-sm-3 text-center">
+          <h1 className="display-4">Login</h1>
+          <Form
+            onSubmit={async e => {
+              e.preventDefault();
+              const res = await login({ variables: { email, password } });
+              console.log(res);
+            }}
+          >
+            <FormGroup>
+              <Input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="email"
+                onChange={e => setEmail(e.target.value)}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="password"
+                onChange={e => setPassword(e.target.value)}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Button type="submit">Submit</Button>
+            </FormGroup>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
+};

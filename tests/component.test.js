5f91c0e25c2ef81d70777409f5b62e13007cd7f7
varsnotwars <varsnotwars@protonmail.com>
2019-09-28 22:56:50 +1000

    Moved dup logic to func

--- tests/component.test.js
+++ tests/component.test.js
@@ -8,22 +8,15 @@ import { RestrictedComponent } from "../src/Components/RestrictedComponent";
 import { AuthProvider } from "../src/Components/AuthProvider";
 
 describe("components:", () => {
-  it("renders restricted profile for authenticated user", async () => {
+  const createAppWithRestrictedComponent = async profileResult => {
     const mocks = {
       request: {
         query: PROFILE
       },
-      result: {
-        data: {
-          profile: {
-            id: "1",
-            email: "test_user@test.com"
-          }
-        }
-      }
+      result: profileResult
     };
 
-    const div = document.createElement("div");
+    const div = document.createElement("root");
 
     ReactDOM.render(
       <MockedProvider mocks={[mocks]} addTypename={false}>
@@ -43,38 +36,25 @@ describe("components:", () => {
       await wait();
     });
 
-    expect(div.getElementsByClassName("restricted").length).not.toBe(0);
+    return div;
+  };
+  it("renders restricted profile for authenticated user", async () => {
+    const profileResult = {
+      data: { profile: { id: "1", email: "test@email.com" } }
+    };
+
+    const app = await createAppWithRestrictedComponent(profileResult);
+
+    expect(app.getElementsByClassName("restricted").length).not.toBe(0);
   });
 
   it("does not render restricted component for unauthenticated user", async () => {
-    const mocks = {
-      request: {
-        query: PROFILE
-      },
-      result: {
-        data: null
-      }
+    const profileResult = {
+      data: null
     };
 
-    const div = document.createElement("div");
+    const app = await createAppWithRestrictedComponent(profileResult);
 
-    ReactDOM.render(
-      <MockedProvider mocks={[mocks]} addTypename={false}>
-        <AuthProvider>
-          <RestrictedComponent>
-            {/* could add the "restricted" class to this element and remove logic from RestrictedComponent */}
-            <div>Restricted content</div>
-          </RestrictedComponent>
-        </AuthProvider>
-      </MockedProvider>,
-      div
-    );
-
-    await act(async () => {
-      // delays until the next "tick" of the event loop,
-      // and allows time for that Promise returned from MockedProvider to be fulfilled
-      await wait();
-    });
-    expect(div.getElementsByClassName("restricted").length).toBe(0);
+    expect(app.getElementsByClassName("restricted").length).toBe(0);
   });
 });

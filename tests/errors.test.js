f28a6c3102472d9bc9574b9daf047fa0f31fba84
varsnotwars <varsnotwars@protonmail.com>
2019-09-30 00:50:21 +1000

    Created test for unconfirmed login

---
+++ tests/errors.test.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { createMemoryHistory } from "history";
+import { Router } from "react-router-dom";
+
+import { MockedProvider, wait } from "@apollo/react-testing";
+import { render, fireEvent } from "@testing-library/react";
+
+import { GraphQLError } from "graphql";
+import { act } from "react-dom/test-utils";
+import { LOGIN } from "../src/graphql/login";
+import { Login } from "../src/Pages/Login";
+
+describe("routes:", () => {
+  it("unconfirmed login catches and displays error", async () => {
+    const history = createMemoryHistory({ initialEntries: ["/login"] });
+    expect(history.location.pathname).toBe("/login");
+
+    const unconfirmedError = "must confirm account";
+
+    const mocks = {
+      request: {
+        query: LOGIN,
+        variables: {
+          email: "email@email.com",
+          password: "password"
+        }
+      },
+      result: {
+        errors: [
+          new GraphQLError(unconfirmedError, null, null, null, null, null, {
+            exception: { name: "UnconfirmedUserError" }
+          })
+        ]
+      }
+    };
+
+    const { getByTestId, getByText } = render(
+      <MockedProvider mocks={[mocks]} addTypename={false}>
+        <Router history={history}>
+          <Login location={{ state: { from: "/", error: null } }} />
+        </Router>
+      </MockedProvider>
+    );
+
+    fireEvent.change(getByTestId("email"), {
+      target: { value: "email@email.com" }
+    });
+    fireEvent.change(getByTestId("password"), {
+      target: { value: "password" }
+    });
+
+    fireEvent.click(getByTestId("login"));
+
+    await act(async () => {
+      await wait(0);
+    });
+
+    expect(getByText(unconfirmedError)).toBeTruthy();
+
+    expect(history.location.pathname).toBe("/login");
+  });
+});
